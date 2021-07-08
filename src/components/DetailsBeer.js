cd5a21bc30470f2f2e103883a379764eb674c8fa
lotiko <l.despres@netc.fr>
2021-07-08 20:13:50 +0200

    start itér 4

--- src/components/DetailsBeer.js
+++ src/components/DetailsBeer.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
 
 export default class DetailsBeer extends React.Component {
-
-    render() {
-        return (<h1>Details</h1>);
-    }
+  state = {
+    beer: false,
+  };
+  componentDidMount() {
+    axios
+      .get(
+        `https://ih-beers-api2.herokuapp.com/beers/${this.props.match.params.id}`
+      )
+      .then((response) => {
+        this.setState({ beer: response.data });
+      });
+  }
+  render() {
+    return (
+      (!this.state.beer && <h1>Loading...</h1>) || (
+        <div className="single-beer">
+          <img src={this.state.beer.image_url} alt="beer" />
+        </div>
+      )
+    );
+  }
 }
